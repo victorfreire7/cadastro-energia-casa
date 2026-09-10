640acf38efe6eb2aa3a093f31e26508851acd33d
victorfreire7 <vhugo.freire159@gmail.com>
2026-09-10 11:00:05 -0300

    add consumoController.js route

--- backend/routes/imovel.js
+++ backend/routes/imovel.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const auth = require('../middlewares/auth');
 const { criar, listar, atualizar, excluir } = require('../controllers/imovelController');
+const { calcular, registrar, historico } = require('../controllers/consumoController');
 
 const router = express.Router();
 
@@ -9,5 +10,8 @@ router.post('/', criar);
 router.get('/', listar);
 router.put('/:id', atualizar);
 router.delete('/:id', excluir);
+router.get('/:id/consumo', calcular);
+router.post('/:id/consumo', registrar);
+router.get('/:id/historico', historico);
 
 module.exports = router;
